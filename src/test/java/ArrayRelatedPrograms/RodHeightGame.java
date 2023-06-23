052b16bc0f42530a0476ebf194e6815c07adf711
8527230165 <goelakshay001gmail.com>
2023-06-23 11:16:16 +0530

    NewCommit

--- src/test/java/ArrayRelatedPrograms/RodHeightGame.java
+++ src/test/java/ArrayRelatedPrograms/RodHeightGame.java
@@ -3,23 +3,25 @@ package ArrayRelatedPrograms;
 public class RodHeightGame {
 
 	public static void main(String[] args) {
-		int[] arr= {1,2,4,3};
-		int w=3;
+		int[] arr= {2,5,4,1,1,3};
+		int w=6;
 		int alex=0;
 		int bob=0;
 		
-		for(int i=0;i<arr.length;i++) {
-			alex=alex+arr[i];
-			bob=bob+arr[i+1];
-			if(alex>3) {
+		for(int i=0;i<arr.length;i=i+2) {
+			alex=alex+arr[i];//1, 1+
+			bob=bob+arr[i+1];//2
+			System.out.println(alex+" , "+bob);
+			if(alex>w) {
 				System.out.println("Alex wins");
 				break;
 			}
-			else if(bob>3) {
+			else if(bob>w) {
 				System.out.println("Bob wins");
 				break;
 			}
-			else {
+			else if(alex==bob){
+				System.out.println(alex+" , "+bob);
 				System.out.println("-1");
 				break;
 			}
